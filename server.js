c899b70a0dd49dd8d9a07c51e99a3dd76eb1bd4f
Stephen Buckley <sebuckley@hotmail.com>
2022-06-20 07:46:26 +0100

    add project files

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+const apiRouter = require('./api/api.js');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(morgan('dev'));
+
+app.use('/api', apiRouter);
+
+app.use(errorhandler());
+
+app.listen(PORT, () => {
+
+console.log(`Listening on port: ${PORT}`);
+
+});
+
+
+module.exports = app;
